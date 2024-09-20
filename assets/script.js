8a7e06e6aeb18cda45d2553a8e37407cdb597bcf
boodac <180095661+Boodac@users.noreply.github.com>
2024-09-20 14:53:34 -0400

    Add form

--- assets/script.js
+++ assets/script.js
@@ -7,13 +7,20 @@ const card = document.createElement("section");
 
 let myLibrary = [];
 
-function Book(bookName, authorName, yearPublished, wordCount) {
+function Book(bookName = "The Default Book", authorName = "boodac", yearPublished = "1973", status = "unread", pageCount = 0, wordCount = 0) {
     this.name = bookName;
     this.author = authorName;
     this.year = yearPublished;
-    this.length = wordCount;
-    this.status = "unread";
+    this.wordCount = wordCount;
+    this.pageCount = pageCount;
+    this.status = status;
     this.id = undefined;
+
+    if(wordCount > 0) {
+        this.length = wordCount;
+    } else if (pageCount > 0) {
+        this.length = pageCount;
+    } else this.length = -1;
 }
 
 function addBookToCollection(bookObject, bookCollection) {
