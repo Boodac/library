20131164007cac8e5868a28fcbad1915973c0316
boodac <180095661+Boodac@users.noreply.github.com>
2024-09-22 07:57:20 -0400

    Add card template to HTML

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,8 @@
-// dom code
 
+// dom code
+const template = document.querySelector("#bookCard");
+console.log(template.content);
 const container = document.querySelector(".container");
-const card = document.createElement("section");
-card.style = "background-color: black; width: 20%; aspect-ratio: 1;"
 const newBookBtn = document.querySelector(".new-book");
 const formContainer = document.querySelector("#form-container");
 const form = document.querySelector("form");
@@ -13,19 +13,27 @@ formContainer.toggle = () => {
 
 submitBtn.addEventListener("click", (e) => {
     formContainer.toggle();
-    console.log(form);
 });
 
 newBookBtn.addEventListener("click", (e) => {
     formContainer.toggle();
 });
 
-
 // library code
 
-let myLibrary = [];
+let myLibrary = {
+    books: [],
+    add: function(bookObject) {
+        if(this.books.includes(bookObject)) return;
+        else { 
+            this.books.push(bookObject); 
+            bookObject.id = this.books.indexOf(bookObject); 
+        }
+    }    
+};
 
-function Book(bookName = "The Default Book", authorName = "boodac", yearPublished = "1973", status = "unread", pageCount = 0, wordCount = 0) {
+function Book(bookName = "The Default Book", authorName = "boodac", yearPublished = "1973", status = "unread", pageCount = 0, wordCount = 0,
+                isbn13 = 1231234567890, genre = "unknown") {
     this.name = bookName;
     this.author = authorName;
     this.year = yearPublished;
@@ -33,6 +41,8 @@ function Book(bookName = "The Default Book", authorName = "boodac", yearPublishe
     this.pageCount = pageCount;
     this.status = status;
     this.id = undefined;
+    this.isbn13 = isbn13;
+    this.genre = genre;
 
     if(wordCount > 0) {
         this.length = wordCount;
@@ -41,7 +51,15 @@ function Book(bookName = "The Default Book", authorName = "boodac", yearPublishe
     } else this.length = -1;
 }
 
-function addBookToCollection(bookObject, bookCollection) {
-    bookCollection.push(bookObject);
-    bookObject.id = bookCollection.indexOf(bookObject);
+const _Pride = new Book("Pride and Prejudice", "Jane Austen", "1813", "unread", 279, 122204, 9780144139518, "romance");
+const _Kill = new Book("To Kill A Mockingbird", "Harper Lee", "1960", "unread", 281, 99121, 9780060935467, "literary fiction");
+const _Farm = new Book("Animal Farm", "George Orwell", "1945", 92 , 29966, 9780452284241, "satire");
+myLibrary.add(_Pride);
+myLibrary.add(_Kill);
+myLibrary.add(_Farm);
+
+// display code
+
+function buildCard(bookArray) {
+    return NodeList;
 }
